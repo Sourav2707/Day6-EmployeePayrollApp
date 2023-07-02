582a2aba275695bbb4e9c8f54ab8fe74df508eb0
Sourav2707 <smartsourav33@gmail.com>
2023-07-02 23:56:09 +0530

    UC6 determining logging levels running in dev, common and prod

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollAppApplication.java
@@ -3,15 +3,15 @@ package com.bridgelabz.employeepayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeepayrollAppApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(EmployeepayrollAppApplication.class, args);
-		log.info("Employee Payroll App Started");
+		ApplicationContext context = SpringApplication.run(EmployeepayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
