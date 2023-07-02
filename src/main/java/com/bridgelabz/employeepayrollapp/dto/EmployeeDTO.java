b392cb0bce2c483f740fc109de39922e425aee15
Sourav2707 <smartsourav33@gmail.com>
2023-07-03 01:53:28 +0530

    UC11 adding remaing properties to the DTO and model

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -5,13 +5,21 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * DTO - Data Transfer Object it is used to carry data between processes.
  * It reduces the number of calls by holding all the data for the call.
  */
 public @Data class EmployeeDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Employee name invalid")
-    private String name;
+    public String name;
     @Min(value = 500, message = "Min wage should be more than 500")
-    private long salary;
+    public long salary;
+    public String gender;
+    public String startDate;
+    public String profilePic;
+    public String note;
+    public List<String> departments;
+
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeData.java
@@ -3,15 +3,31 @@ package com.bridgelabz.employeepayrollapp.model;
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import lombok.Data;
 
-@Data
-public class EmployeeData {
+import java.time.LocalDate;
+import java.util.List;
+
+public @Data class EmployeeData {
     private int empID;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> departments;
 
     public EmployeeData(int empID, EmployeeDTO employeeDTO) {
         this.empID = empID;
         this.name = employeeDTO.getName();
         this.salary = employeeDTO.getSalary();
+        this.gender = employeeDTO.getGender();
+        this.startDate = LocalDate.parse(employeeDTO.getStartDate());
+        this.note = employeeDTO.getNote();
+        this.profilePic = employeeDTO.getProfilePic();
+        this.departments = employeeDTO.getDepartments();
     }
+    public EmployeeData() {
+
+    }
+
 }
